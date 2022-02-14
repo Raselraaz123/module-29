5304c31ffba3f7827e2852b5464437c6ea016ec5
Raselraaz123 <82328669+Raselraaz123@users.noreply.github.com>
2022-02-14 16:11:00 +0600

    three commit

---
+++ three.js
@@ -0,0 +1,19 @@
+const frindsName = 'Rasel Roman Rohan Rabaya Dudu Aisha';
+const allfrinds = frindsName.split(" ")
+const manyFrind = frindsName.split("a")
+const someone = frindsName.slice(0, 7)
+const anething = frindsName.substr(7, 15)
+const tomato = frindsName.substring(0, 17)
+
+// console.log(manyFrind);
+// console.log(someone);
+// console.log(anething);
+// console.log(tomato);
+
+let many = "rasel roman rajo raihan ";
+let nane = "jamo ga ame ";
+let allmany = many.concat(nane).concat('amadar bare jaba');
+// console.log(allmany);
+const word = ['a', 'b', 'c', 'd', 'e']
+const allWord = word.join(' ,? ');
+console.log(allWord)
