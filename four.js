431667b781a54bd7c686d765011ad0437b5a8fad
Raselraaz123 <82328669+Raselraaz123@users.noreply.github.com>
2022-02-15 09:24:04 +0600

    all ok

--- four.js
+++ four.js
@@ -20,3 +20,12 @@ if (frindsName.includes('roman')) {
     console.log('roman in')
 
 }
+// const task = "Practice Makes a person perfect";
+// const index = task.toLowerCase().indexOf("M");
+// console.log(index)
+// const str1 = 'Bangladesh is a beautiful country.';
+// console.log(str1.endsWith("."));
+// for (let i== 0; i < 10; i++) { }
+// const array = ["121", "12", "1", "112", "111"];
+
+// console.log(array.sort());
